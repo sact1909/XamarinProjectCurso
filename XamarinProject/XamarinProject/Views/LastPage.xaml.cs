1e45545a695b3ebb98ec18db831548224838c1a2
Steven Checo <steven.checo.19@gmail.com>
2020-06-17 20:13:56 -0400

    first commit

---
+++ XamarinProject/XamarinProject/Views/LastPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinProject.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LastPage : ContentPage
+    {
+        public LastPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Shell.Current.Navigation.PopAsync();
+        }
+    }
+}
